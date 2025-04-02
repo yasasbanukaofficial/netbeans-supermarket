06d2c4ed8878a845ef1892b45a8d5a596de5f559
unknown <yasasbanukaofficial@gmail.com>
2025-04-03 00:20:11 +0530

    feat: initialize UI in Main.java

--- src/edu/ijse/supermarket/Main.java
+++ src/edu/ijse/supermarket/Main.java
@@ -1,8 +1,11 @@
-
 package edu.ijse.supermarket;
+import edu.ijse.supermarket.view.CustomerView;
+import edu.ijse.supermarket.view.ItemView;
+import java.sql.SQLException;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException, SQLException{
+        new ItemView().setVisible(true);
+        new CustomerView().setVisible(true);
     }
-    
 }
