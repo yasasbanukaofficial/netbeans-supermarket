7f8f9d66d3f198815a1c5bafd95d35a1099f01f8
unknown <yasasbanukaofficial@gmail.com>
2025-04-02 23:38:52 +0530

    chore: added base directories

--- src/edu/ijse/supermarket/Main.java
+++ src/edu/ijse/supermarket/Main.java
@@ -1,20 +1,8 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
+
 package edu.ijse.supermarket;
 
-/**
- *
- * @author ASUS
- */
 public class Main {
-
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
     }
     
 }
